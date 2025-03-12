a22a3310a11c7cac4597729e613c3263a69f1840
duonghoangchau <143429168+duonghoangchau@users.noreply.github.com>
2025-03-12 14:31:38 +0700

    insert seeder: product + updateDiscountPrice

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -23,6 +24,8 @@ class DatabaseSeeder extends Seeder
         $this->call(ProvinceSeeder::class);
         $this->call(DistrictsSeeder::class);
         $this->call(UpdateDistrictFeeSeeder::class);
+        $this->call(ProductSeeder::class);
+        $this->call(UpdateDiscountPriceSeeder::class);
 
 
 
